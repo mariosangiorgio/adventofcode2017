8aeb44b305dec61a2d69af2433e2986467085fff
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-12-01 23:04:46 +0000

    Unified solutions

--- 01/inverse_captcha.jl
+++ 01/inverse_captcha.jl
@@ -1,41 +1,28 @@
 using Base.Test
 
-function part1(chars)
+function solve(chars, offset::Int)
   sum = 0
   len = length(chars)
-  for i in 1:len
-    if chars[i] == chars[i%len + 1]
-      sum += parse(Int, chars[i])
-    end
-  end
-  sum
-end
-
-@test part1("1122") == 3
-@test part1("1234") == 0
-@test part1("91212129") == 9
-
-function part2(chars)
-  sum = 0
-  len = length(chars)
-  offset = Int(len/2)
   for i in 1:len
     # 1-base indexing is a bit weird for this
-    if chars[i] == chars[(i -1 + offset) % len + 1]
+    if chars[i] == chars[(i - 1 + offset)%len + 1]
       sum += parse(Int, chars[i])
     end
   end
   sum
 end
 
-@test part2("1212") == 6
-@test part2("1221") == 0
-@test part2("123425") == 4
-@test part2("123123") == 12
-@test part2("12131415") == 4
+@test solve("1122", 1) == 3
+@test solve("1234", 1) == 0
+@test solve("91212129", 1) == 9
+@test solve("1212", 2) == 6
+@test solve("1221", 2) == 0
+@test solve("123425", 3) == 4
+@test solve("123123", 3) == 12
+@test solve("12131415", 4) == 4
 
 open("/Users/mariosangiorgio/Downloads/input") do input
   chars = strip(readstring(input))
-  println(part1(chars))
-  println(part2(chars))
+  println(solve(chars, 1))
+  println(solve(chars, length(chars)/2))
 end
