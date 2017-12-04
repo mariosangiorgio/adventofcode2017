342e5fedac9e3c6b6e18eae81c5ec214b85456bf
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-12-04 21:31:48 +0000

    Day 4, part 2

--- 04/high_entropy_passwords.pl
+++ 04/high_entropy_passwords.pl
@@ -1,15 +1,23 @@
 use strict;
 use warnings;
+use feature qw(say);
 use List::MoreUtils qw(uniq);
 
 open my $handle, '<', "/Users/mariosangiorgio/Downloads/input";
 chomp(my @lines = <$handle>);
-my $valid = 0;
+my $valid1 = 0;
+my $valid2 = 0;
 foreach my $line (@lines){
   my @words = split / /, $line;
-  if((scalar @words) == (scalar uniq @words)){
-    $valid++;
+  my $n = scalar @words;
+  if($n == (scalar uniq @words)){
+    $valid1++;
+  }
+  my @canonical_words = map {join '', sort split //, $_ } @words;
+  if($n == (scalar uniq @canonical_words)){
+    $valid2++;
   }
 }
-print $valid;
+say $valid1;
+say $valid2;
 close $handle;
