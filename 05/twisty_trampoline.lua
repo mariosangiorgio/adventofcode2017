b5b08c6dfe06f8a9602d47ec833dbaf38374ede1
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-12-05 22:03:44 +0000

    Day 5, part 2

--- 05/twisty_trampoline.lua
+++ 05/twisty_trampoline.lua
@@ -1,18 +1,32 @@
 local function read_file(path)
     local lines = {}
     for line in io.lines(path) do
-      lines[#lines + 1] = tonumber(line) // index starts at 1
+      lines[#lines + 1] = tonumber(line) -- index starts at 1
     end
     return lines
 end
 
-local program_counter = 1
-local steps = 0
-local memory = read_file("/Users/mariosangiorgio/Downloads/input")
-while(program_counter <= #memory)do
-  local jump = memory[program_counter]
-  memory[program_counter] = jump + 1
-  program_counter = program_counter + jump
-  steps = steps + 1
+local function interpret(memory, editor)
+  local program_counter = 1
+  local steps = 0
+  while(program_counter <= #memory)do
+    local jump = memory[program_counter]
+    memory[program_counter] = editor(jump)
+    program_counter = program_counter + jump
+    steps = steps + 1
+  end
+  return steps
 end
+
+local function part2editor(v)
+  if(v >=3)
+    then return v-1
+    else return v+1
+  end
+end
+
+local memory = read_file("/Users/mariosangiorgio/Downloads/input")
+-- part 1 local steps = interpret(memory, function(v)return v+1 end)
+-- part 2
+local steps = interpret(memory, part2editor)
 print (steps)
