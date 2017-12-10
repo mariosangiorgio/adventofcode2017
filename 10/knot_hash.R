a82c527be0bdb49dd26633569e8c3fab1b46819a
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-12-10 23:55:36 +0000

    Day 10, part 2

--- 10/knot_hash.R
+++ 10/knot_hash.R
@@ -18,3 +18,18 @@ for (i in seq_along(input)){
 	c <- (c + (i-1) + input[i]) %% length(s) 
 }
 print(s[1]*s[2])
+
+input <- scan('/Users/mariosangiorgio/Downloads/input', what="")
+input <- c(as.integer(charToRaw(input)), c(17, 31, 73, 47, 23))
+s <- seq(0,255)
+c <- 0
+skip <- 0
+for(iteration in seq(1,64)){
+	for (i in seq_along(input)){	
+		s <- shuffle(s, c, input[i])
+		c <- (c + skip + input[i]) %% length(s) 
+		skip <- skip + 1
+	}	
+}
+hash <- lapply(seq(0,15), function(x){Reduce(bitwXor,s[seq(1+16*x,16*(x+1))])})
+print(paste(as.raw(hash), collapse=""))
