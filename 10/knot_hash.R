eb4e6091a33d05040221ebab14e963e2fa308099
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-12-14 21:47:28 +0000

    Input generator for day 14

--- 10/knot_hash.R
+++ 10/knot_hash.R
@@ -19,17 +19,26 @@ for (i in seq_along(input)){
 }
 print(s[1]*s[2])
 
-input <- scan('/Users/mariosangiorgio/Downloads/input', what="")
-input <- c(as.integer(charToRaw(input)), c(17, 31, 73, 47, 23))
-s <- seq(0,255)
-c <- 0
-skip <- 0
-for(iteration in seq(1,64)){
-	for (i in seq_along(input)){	
-		s <- shuffle(s, c, input[i])
-		c <- (c + skip + input[i]) %% length(s) 
-		skip <- skip + 1
-	}	
+hash <- function(input){
+	input <- c(as.integer(charToRaw(input)), c(17, 31, 73, 47, 23))
+	s <- seq(0,255)
+	c <- 0
+	skip <- 0
+	for(iteration in seq(1,64)){
+		for (i in seq_along(input)){	
+			s <- shuffle(s, c, input[i])
+			c <- (c + skip + input[i]) %% length(s) 
+			skip <- skip + 1
+		}	
+	}
+	paste(as.raw(lapply(seq(0,15), function(x){Reduce(bitwXor,s[seq(1+16*x,16*(x+1))])})), collapse="")
 }
-hash <- lapply(seq(0,15), function(x){Reduce(bitwXor,s[seq(1+16*x,16*(x+1))])})
-print(paste(as.raw(hash), collapse=""))
+
+input <- scan('/Users/mariosangiorgio/Downloads/input', what="")
+print(hash(input))
+
+# Input for day 14
+lines <- lapply(seq(0,127), function(i){hash(paste("hxtvlmkl-", i, sep=""))})
+fileConn<-file("/Users/mariosangiorgio/Downloads/output")
+writeLines(Reduce(function(a,b){paste(a,b,"\n",sep="")},lines), fileConn)
+close(fileConn)
