82b668732b84d9a30e36edf19c2f7f1bb903f91e
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-12-17 20:52:01 +0000

    Day 17, part 2

--- 17/spinlock.erl
+++ 17/spinlock.erl
@@ -1,5 +1,5 @@
 -module(spinlock).
--export([main/0, spinlock/5, item_after/2]).
+-export([main/0, spinlock/5, spinlock2/5, item_after/2]).
 
 
 spinlock(List, _Steps, _Current, ToInsert, Target) when  ToInsert > Target ->
@@ -8,6 +8,18 @@ spinlock(List, Steps, Current, ToInsert, Target) ->
   Destination = (Current + Steps) rem length(List) + 1,
   {Before, After} = lists:split(Destination, List),
   spinlock(Before ++ [ToInsert | After], Steps, Destination, ToInsert + 1, Target).
+
+spinlock2(Count, _Steps, _Current, ToInsert, Target) when  ToInsert > Target ->
+  Count;
+spinlock2(Count, Steps, Current, ToInsert, Target) ->
+  Destination = (Current + Steps) rem Count + 1,
+  if
+    Destination == 1 ->
+      io:format("~w ~w~n", [Count, ToInsert]);
+    true ->
+      ok
+  end,
+  spinlock2(Count + 1, Steps, Destination, ToInsert + 1, Target).
 
 %% Note what happens when the item to we want is the last
 item_after(ToFind, [H,I|_Tail]) when ToFind == H ->
