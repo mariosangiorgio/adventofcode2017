2fb724b4c659a23df3cf27d87108e3360ca621ea
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-12-20 21:45:17 +0000

    Cleaned part 1

--- 20/particle_swarm.exs
+++ 20/particle_swarm.exs
@@ -1,8 +1,14 @@
 # d(t) = |sx + vx*t +ax*t*(t+1)/2| + |sy + vy*t +ay*t*(t+1)/2| |sy + vy*t +ay*t*(t+1)/2|
-# For t -> +inf O(d(t))= (|ax| + |ay| |ay|)*t*(t+1)/2
+lines =
 File.stream!("/Users/mariosangiorgio/Downloads/input") |>
 Stream.map( &(String.replace(&1, "\n", "")) ) |>
 Stream.with_index |>
+Enum.map(fn({line, index})->{line, index} end)
+# Part 1
+# For t -> +inf O(d(t))= (|ax| + |ay| |ay|)*t*(t+1)/2
+# Assumes no ties in the accelerations
+[{index, _}|t]=
+lines |>
 Enum.map( fn({line, index}) ->
   [p, v, a] = String.split(line, " ")
   [ax, ay, az] = String.slice(a,3..-2) |> String.split(",") |> Enum.map(&String.to_integer(&1))
@@ -10,3 +16,4 @@ Enum.map( fn({line, index}) ->
   {index, cost}
 end) |>
 Enum.sort_by(fn({_, cost}) -> cost end)
+IO.write(index)
