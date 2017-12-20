90fc1d6bc5eafb48065891b10083b01989d5444f
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-12-20 21:55:56 +0000

    Factored out parsing

--- 20/particle_swarm.exs
+++ 20/particle_swarm.exs
@@ -1,19 +1,26 @@
 # d(t) = |sx + vx*t +ax*t*(t+1)/2| + |sy + vy*t +ay*t*(t+1)/2| |sy + vy*t +ay*t*(t+1)/2|
-lines =
+defmodule ParticleSwarm do
+  def parse(token) do
+    String.slice(token,3..-2) |> String.split(",") |> Enum.map(&String.to_integer(&1))
+  end
+end
+input =
 File.stream!("/Users/mariosangiorgio/Downloads/input") |>
 Stream.map( &(String.replace(&1, "\n", "")) ) |>
 Stream.with_index |>
-Enum.map(fn({line, index})->{line, index} end)
+Enum.map(fn({line, index})->
+  [p, v, a] = String.split(line, ", ")
+  {index, {ParticleSwarm.parse(p), ParticleSwarm.parse(v), ParticleSwarm.parse(a)}}
+end)
 # Part 1
 # For t -> +inf O(d(t))= (|ax| + |ay| |ay|)*t*(t+1)/2
 # Assumes no ties in the accelerations
 [{index, _}|t]=
-lines |>
-Enum.map( fn({line, index}) ->
-  [p, v, a] = String.split(line, " ")
-  [ax, ay, az] = String.slice(a,3..-2) |> String.split(",") |> Enum.map(&String.to_integer(&1))
-  cost = [ax, ay, az] |> Enum.map(&abs/1) |> Enum.sum
+input |>
+Enum.map( fn({index, {_,_,a}}) ->
+  cost = a |> Enum.map(&abs/1) |> Enum.sum
   {index, cost}
 end) |>
 Enum.sort_by(fn({_, cost}) -> cost end)
 IO.write(index)
+# Part 2
